e1adc1485c135dafcf97cbe7bd61a79ff2d089a6
KenzieNimmo <45235156+KenzieNimmo@users.noreply.github.com>
2020-02-25 17:04:10 +0100

    Update fit_bursts_fb.py

--- fit_bursts_fb.py
+++ fit_bursts_fb.py
@@ -8,8 +8,7 @@ from fit_repeater_bursts import fit_my_smudge
 import filterbank
 import os
 import pickle
-
-
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     path = './'
@@ -31,33 +30,32 @@ if __name__ == '__main__':
         t_samp = fil.header['tsamp']
 
         arr = filterbank_to_arr.filterbank_to_np(filfile,maskfile,total_N)
-        amask_file="./mask.txt"
+        amask_file=None
         if amask_file!=None:
             amaskfile = np.loadtxt(amask_file)
             amask=[int(i) for i in amaskfile]
             arr[amask,:]=0
-        spec_bp_corr = filterbank_to_arr.bandpass(arr, t_samp) #bandpass correct
-
+        arr_burst = filterbank_to_arr.bandpass(arr, t_samp) #bandpass correct
+        
+        
         #properties of filterbank file
         freqs = np.flip(fil.frequencies)
         f_res = (freqs[-1]-freqs[0])/(fil.header['nchans']-1)
-        stimes = np.linspace(0,total_N,total_N)
+        stimes = np.linspace(0,arr_burst.shape[1],arr_burst.shape[1])
         stimes*=t_samp
         #guess=[dmax, xmax, ymax, xwid, ywid, 0]
         if burst == "4":
-            guess = [50,430,4000,4,8000,0]
+            guess = [50,450,4000,4,8000,0]
         else: guess = []
 
-        bin_times,fit, bin_center = fit_my_smudge(arr, stimes, freqs, guess=guess, doplot=True, basename=filname)
-        fits = {}
+        bin_times,fit, bin_center = fit_my_smudge(arr_burst, stimes, freqs, guess=guess, doplot=True, basename=filname)
+        fits={}
         burst_properties={}
-        fits['centre_bin']=np.int(np.ceil(bin_times[0]))
+        fits['centre_bin']=bin_times[0]
         fits['width_bin']=bin_times[1]
-
-        burst_properties[burst] = fits
-    f=open("burst_peaks_widths.pkl", "wb")
+        
+        burst_properties[burst] = fits 
+    f=open("bursts_peak_width.pkl", "wb")
     pickle.dump(burst_properties,f)
     f.close()
-
-        #fits[str(burst)] = fit[3:-1]
-        #bin_centers = np.vstack([bin_centers,bin_center])
+        
