1c84fb7ed3bdf94265b1b094de0e33020e5c38d0
KenzieNimmo <45235156+KenzieNimmo@users.noreply.github.com>
2020-02-25 17:04:54 +0100

    include bandpass correction

--- filterbank_to_arr.py
+++ filterbank_to_arr.py
@@ -13,10 +13,29 @@ def filterbank_to_np(filename, maskfile, total_N):
         maskchans = mask.mask_zap_chans
         maskchans = fil.header['nchans']-1-np.array(list(maskchans))
         arr[maskchans,:]=0
-    return arr, t_samp
+    arr=np.flip(arr,0)
+    return arr
 
-def spectra(numpy_arr,burst_cent,burst_width):
+def burst_spectra(numpy_arr,burst_cent,burst_width):
     burst_begin = int(np.ceil(burst_cent-(burst_width/2.)))
     burst_end = int(np.ceil(burst_cent+(burst_width/2.)))
     spec = np.mean(numpy_arr[:,burst_begin:burst_end],axis=1)
     return spec
+
+def bandpass(arr,t_res):
+    """
+    Uses off pulse data (excluding 20ms around the burst) to normalise the spectrum and
+    calibrate the bandpass
+    """
+    offset = int(10e-3/t_res)
+    spectrum = np.mean(arr,axis=1)
+    good_bins = spectrum!=0
+    profile = np.mean(arr,axis=0)
+    off_times = np.ones(arr.shape[1],np.bool)
+    bin_peak = np.where(profile==np.amax(profile))[0][0]
+    off_times[np.int(bin_peak-offset):np.int(bin_peak+offset)]=False
+    offspec = np.mean(arr[:,off_times],axis=1)
+    for i in range(arr.shape[1]):
+        arr[:,i]/=offspec
+    arr[~good_bins,:]=0
+    return arr
