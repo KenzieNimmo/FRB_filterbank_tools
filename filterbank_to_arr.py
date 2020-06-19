1345e55ab112da594ef6cc5037f65cb50edbae0b
KenzieNimmo <45235156+KenzieNimmo@users.noreply.github.com>
2020-06-19 11:35:07 +0200

    smoothing

--- filterbank_to_arr.py
+++ filterbank_to_arr.py
@@ -28,12 +28,14 @@ def filterbank_to_np(filename, dm=None, maskfile=None, bandpass=False, offpulse=
         arr=bp(filename,maskfile,nbins,offpulse)
     return arr
 
-def fits_to_np(filename, dm=None, maskfile=None, bandpass=False, offpulse=None, nbins=6,AO=False):
+def fits_to_np(filename, dm=None, maskfile=None, bandpass=False, offpulse=None, n\
+bins=6,AO=False):
     fits=psrfits.PsrfitsFile(filename)
     total_N=fits.specinfo.N
     t_samp=fits.specinfo.dt
     if AO==True:
-        peak_bin=(total_N/10.)*2 #the chopped filterbank is 10 subints (2 before the burst and 8 after)
+        peak_bin=(total_N/10.)*2 #the chopped filterbank is 10 subints (2 before \
+the burst and 8 after)
         #+-0.1seconds
         begin_bin=int(peak_bin-(0.1/t_samp)) #number of bins
         end_bin=int(peak_bin+(0.1/t_samp))
@@ -56,14 +58,68 @@ def fits_to_np(filename, dm=None, maskfile=None, bandpass=False, offpulse=None, 
         arr=bp(filename,maskfile,nbins,offpulse)
     return arr
 
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+
+    see also:
+
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
+
 
 
 def bp(filename,maskfile,nbins,offpulsefile,kind='slinear'):
     """
     Uses off pulse data (identified using interactive RFI_masker.py) to normalise the spectrum and
     calibrate the bandpass
-
-    Note the spline fit is not working. It introduces weird artefacts. Currently just simply dividing without smoothing.
     """
     fil = filterbank.filterbank(filename)
     total_N = fil.number_of_samples
@@ -72,35 +128,25 @@ def bp(filename,maskfile,nbins,offpulsefile,kind='slinear'):
     arr = np.array([spec[i] for i in xrange(fil.header['nchans'])])
     offpulse=pickle.load(open(offpulsefile,'rb'))
     spec = np.mean(arr[:,offpulse],axis=1)
-    #means=np.mean(spec[1:-1].reshape(-1, nbins), axis=1)
-    #meanint = np.append(spec[1],means)
-    #meanspec = np.append(meanint,spec[-1])
-    #x1 = np.append(0,np.arange(1,nchans-2,nbins))
-    #x = np.append(x1,nchans)
-    #cs = interp(x, meanspec, kind=kind)
-    #fit = cs(np.arange(0,nchans,1))
+    # smooth the bandpass spectrum
+    spec_smooth = smooth(spec,window_len=10)
+
+    plt.plot(spec)
+    plt.plot(spec_smooth)
+    plt.show()
+
     if maskfile!=None:
         amaskfile = pickle.load(open(maskfile,'rb'))
         amask=[int(i) for i in amaskfile]
-        amask = np.array(amask)
-        #vmin=np.amin(fit)
-        #fit[amask]=vmin-100
-        #mask = fit<vmin-50
-        #maskfit = np.ma.masked_where(mask==True,fit)
-        vmin = np.amin(arr)
-        arr[amask,:]=vmin-100
-        maskar = arr<vmin-50
-        arr = np.ma.masked_where(maskar==True,arr)
-    #else:
-        #maskfit = fit
+        maskarr=np.zeros_like(arr)
+        maskarr[amask]=1
+        arr = np.ma.masked_where(maskarr==True,arr)
     arr2=arr.copy()
     for i in range(arr.shape[1]):
-        arr2[:,i]/=spec
+        arr2[:,i]/=spec_smooth
         #arr[:,i]/=maskfit
 
-    #plt.plot(np.mean(arr,axis=1),'b')
-    #plt.plot(np.mean(arr2,axis=1),'r')
-    #plt.show()
-
     arr2-=np.mean(arr2)
+    plt.plot(np.mean(arr2,axis=1))
+    plt.show()
     return arr2
